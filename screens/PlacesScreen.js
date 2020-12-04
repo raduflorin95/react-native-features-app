a1022d790d320c3a7dbe12953cf10841af620cc2
Radu Florin <raduflorin95@MacBook-Air-Radu.local>
2020-12-04 20:32:55 +0200

    Added navigation

---
+++ screens/PlacesScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const PlacesScreen = (props) => {
+  return (
+    <View>
+      <Text>Places Listing Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default PlacesScreen;
